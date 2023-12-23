bb04fb6b96967cef5c824ca6ac1049e3c6f0a5be
Mo <>
2023-12-23 11:40:10 +0100

    add home view

---
+++ Resolver/SampleApp/SampleApp/Views/HomeView.swift
@@ -0,0 +1,54 @@
+import Foundation
+import SwiftUI
+import Resolver
+
+public struct HomeView: View {
+    private let mediaContentSections: [MediaContentSection]
+    private let provider: Provider
+
+    private var gridItems: [GridItem] {
+        [GridItem(.adaptive(minimum: 180), spacing: 5)]
+    }
+    public init(mediaContentSections: [MediaContentSection], provider: Provider) {
+        self.mediaContentSections = mediaContentSections
+        self.provider = provider
+    }
+
+    public var body: some View {
+        ScrollView {
+            LazyVGrid(columns: gridItems) {
+                
+                ForEach(mediaContentSections, id: \.self) { section in
+                    Section(header: Text(section.title).font(.title3)) {
+                        
+                        ForEach(section.media, id: \.self) { media in
+                            VStack {
+                                NavigationLink(value: media) {
+                                    AsyncImage(
+                                        url: media.posterURL,
+                                        content: { image in
+                                            image.resizable()
+                                                .aspectRatio(contentMode: .fit)
+                                                .frame(maxWidth: 90)
+                                        },
+                                        placeholder: {
+                                            ProgressView()
+                                        }
+                                    )            }
+                                Text(media.title)
+                            }
+                        }
+                    }.padding(.horizontal)
+
+                }
+            }.navigationDestination(for: MediaContent.self) { media in
+                if media.type == .movie {
+                    MoviesDetailsView(url: media.webURL, provider: provider)
+                } else {
+                    TVShowDetailsView(url: media.webURL, provider: provider)
+                }
+            }
+        }
+    }
+
+}
--- Resolver/SampleApp/SampleApp/Views/MainView.swift
+++ Resolver/SampleApp/SampleApp/Views/MainView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 import Resolver
 
 public struct MainView: View {
+    @State private var homeMediaContentSection: [MediaContentSection] = []
     @State private var tvMediaContent: [MediaContent] = []
     @State private var moviesMediaContent: [MediaContent] = []
     private let provider: Provider
@@ -14,6 +15,12 @@ public struct MainView: View {
     public var body: some View {
 
         TabView {
+            NavigationStack {
+                HomeView(mediaContentSections: homeMediaContentSection, provider: provider)
+                    .navigationTitle("Home")
+            } .tabItem {
+                Text("Home")
+            }
             NavigationStack {
                 ListingView(mediaContent: tvMediaContent, provider: provider)
                     .navigationTitle("TV shows")
@@ -34,6 +41,7 @@ public struct MainView: View {
     @Sendable
     private func refreshTask() {
         Task {
+            self.homeMediaContentSection = try await provider.home()
             self.tvMediaContent = try await provider.latestTVShows(page: 1)
             self.moviesMediaContent = try await provider.latestMovies(page: 1)
         }
