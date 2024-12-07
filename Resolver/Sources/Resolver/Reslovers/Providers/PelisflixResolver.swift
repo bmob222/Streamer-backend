97579b0cde58ac737fd611dc9acc683f4f880c84
HereThereEveryWhere <38498380+bmob222@users.noreply.github.com>
2024-12-07 18:58:59 -0500

    Update PelisflixResolver.swift

--- Resolver/Sources/Resolver/Reslovers/Providers/PelisflixResolver.swift
+++ Resolver/Sources/Resolver/Reslovers/Providers/PelisflixResolver.swift
@@ -3,7 +3,35 @@ import SwiftSoup
 
 struct PelisflixResolver: Resolver {
     let name = "Pelisnetflix"
-    static let domains: [String] = ["pelisflix.one", "pelisflix.uno", "pelisflix.org", "pelisflix2.tv", "pelisflix2.bio"]
+     static let domains: [String] = [
+        "pelisflix.one",
+        "pelisflix.uno",
+        "pelisflix.org",
+        "pelisflix2.tv",
+        "pelisflix2.bio",
+        "pelisplushd.nz",
+        "peliculasmegadrive.com",
+        "peliculasmp4hd.com",
+        "peliculaspornogratisxxx.com",
+        "peliculaszi.com",
+        "pelis24.gratis",
+        "pelis247.org",
+        "pelis28.lol",
+        "pelisenhd.org",
+        "pelismegahd.pe",
+        "pelisonline.me",
+        "pelisonline.ws",
+        "pelispe.com",
+        "pelispedia.la",
+        "pelisplus.icu",
+        "pelisplus.online",
+        "pelisplus.uno",
+        "pelisplushd.so",
+        "pelisplustv.live",
+        "pelistorrent.re",
+        "pelisplushd.bz",
+       
+    ]
 
     func getMediaURL(url: URL) async throws -> [Stream] {
         let pageData = try await Utilities.downloadPage(url: url, encoding: .isoLatin1)
